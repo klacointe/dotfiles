9be304f2e5510df1bff0260ed28a0f770d829ae8
klacointe <kevinlacointe@gmail.com>
2022-02-05 18:07:43 +0100

    feat: export aws credential in zsh

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -14,4 +14,5 @@ path=(
 
 export VISUAL=nvim
 export EDITOR=nvim
-
+export AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
+export AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
